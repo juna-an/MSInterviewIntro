fb3ccff16a4d308b63a792ea395a271944b55d7d
juna-an <juna.andrea18@gmail.com>
2023-08-29 21:20:36 +0300

    fixed an issue when creating the blobs and added a deletion part for blobs from source SA

--- moveScript.ps1
+++ moveScript.ps1
@@ -16,30 +16,39 @@ $numOfBlobs = 3
 # Specify the file path
 $filePath = "file.txt"
 # Write the string to the file
-"hello world" | Out-File -FilePath $filePath
+"hello world454637" | Out-File -FilePath $filePath
 
 # Create $numOfBlobs blobs in source storage account
 Write-Host "**************************************************************"
 Write-Host "******************** Statr creating blobs ********************"
+Write-Host -NoNewline "Creating blob: "
 for ($i = 1; $i -le $numOfBlobs; $i++) {
     $blobName = "blob$i.txt"
-    Write-Host -NoNewline "Creating blob: $blobName, "
+    Write-Host -NoNewline "$blobName, "
+    $sourceBlob = Get-AzStorageBlob -Context $srcContext `
+        -Container $srcContainerName `
+        -Blob $blobName `
+        -ErrorAction SilentlyContinue
+
     $setBlobCommand = Set-AzStorageBlobContent -Container $srcContainerName `
         -Blob $blobName `
         -BlobType Block `
         -File $filePath `
-        -Context $srcContext
+        -Context $srcContext `
+        -Force
 }
 Write-Host ""
 Write-Host "****************** Finished creating blobs! ******************"
 Write-Host "**************************************************************"
+Write-Host ""
 
 # Copy blobs from source storage account to destination storage account
 Write-Host "**************************************************************"
 Write-Host "********************* Statr copying blobs ********************"
+Write-Host -NoNewline "Copying blobs: "
 for ($i = 1; $i -le $numOfBlobs; $i++) {
     $blobName = "blob$i.txt"
-    Write-Host -NoNewline "Copying blob: $blobName, "
+    Write-Host -NoNewline "$blobName, "
     $sourceUri = "https://$sourceStorageAccount.blob.core.windows.net/$srcContainerName/$blobName"
     $sourceBlob = Get-AzStorageBlob -Context $srcContext `
         -Container $srcContainerName `
@@ -51,13 +60,38 @@ for ($i = 1; $i -le $numOfBlobs; $i++) {
         -SrcBlob $sourceBlob.Name `
         -SrcContainer $srcContainerName `
         -DestContainer $destContainerName `
-        -DestBlob $blobName
+        -DestBlob $blobName `
+        -Force
 
     # Monitor the copy status if needed
-    $copyStatus = $destinationBlob | Get-AzStorageBlobCopyState
-    $copyStatus.Status
+    # $copyStatus = $destinationBlob | Get-AzStorageBlobCopyState
+    # $copyStatus.Status
 
 }
 Write-Host ""
 Write-Host "******************* Finished copying blobs! ******************"
 Write-Host "**************************************************************"
+Write-Host ""
+
+Write-Host "**************************************************************"
+Write-Host "**************** Statr deleting blobs from SA A **************"
+Write-Host -NoNewline "Deleting all blobs in source storage account!"
+for ($i = 1; $i -le $numOfBlobs; $i++) {
+    $blobName = "blob$i.txt"
+    Remove-AzStorageBlob -Context $srcContext -Container $srcContainerName -Blob $blobName
+}
+
+Write-Host ""
+Write-Host "*************** Finished deleting blobs from SA A ************"
+Write-Host "**************************************************************"
+
+# # Delete a container recursivly
+# # List blobs in the container
+# $blobs = Get-AzStorageBlob -Context $srcContext -Container $srcContainerName
+# # Delete each blob in the container
+# foreach ($blob in $blobs) {
+#     Remove-AzStorageBlob -Context $srcContext -Container $srcContainerName -Blob $blob.Name -Force
+# }
+
+# # Delete the now-empty container
+# Remove-AzStorageContainer -Name $srcContainerName -Context $srcContext -Force
